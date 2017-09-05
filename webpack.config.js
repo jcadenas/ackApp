465b4a7c39c601cdcd19799d3633b00e4299a528
julian <julian.cadenas@gmail.com>
2017-09-04 21:52:35 -0400

    Remove minification warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
 const path = require('path');
+
+let webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+let devPlugins = []; // if using any plugins for development
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+
 module.exports = {
   context: __dirname,
   entry: './frontend/index.jsx',
@@ -9,6 +33,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
